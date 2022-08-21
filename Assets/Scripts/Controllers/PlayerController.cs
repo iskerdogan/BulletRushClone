93ed2812ff2db847b5a38df225b34cf3ca9d7c5f
Erdoğan IŞIK <erdogani0202@gmail.com>
2022-08-21 21:17:20 +0300

    bug fixed and camera controller added.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,31 +5,73 @@ using Utilities;
 
 public class PlayerController : MyCharacterController
 {
-    private void FixedUpdate() 
+    private bool isShooting;
+    private List<Transform> enemies = new List<Transform>();
+
+    private void Update()
+    {
+        if (enemies.Count>0)
+        {
+            transform.LookAt(enemies[0]);
+        }
+    }
+
+    private void FixedUpdate()
     {
         var direction = new Vector3(TouchController.Instance.Direction.x, 0, TouchController.Instance.Direction.y);
         Move(direction);
     }
 
-    private void OnCollisionEnter(Collision other) 
+    private void OnCollisionEnter(Collision other)
     {
-        var enemy= other.transform.GetComponent<EnemyController>();
+        var enemy = other.transform.GetComponent<EnemyController>();
         if (enemy != null)
         {
             Dead();
         }
     }
 
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerStay(Collider other)
     {
-        var enemy= other.transform.GetComponent<EnemyController>();
+        var enemy = other.transform.GetComponent<EnemyController>();
         if (enemy != null)
         {
-            var direction = other.transform.position - transform.position;
-            direction.y = 0;
-            direction = direction.normalized;
-            ShootController.Instance.Shoot(direction,transform.position);
-        }    
+            if (!enemies.Contains(other.transform)) enemies.Add(other.transform);
+            AutoShoot();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var enemy = other.transform.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            enemies.Remove(other.transform);
+        }
+    }
+
+    private void AutoShoot()
+    {
+        IEnumerator Do()
+        {
+            while (enemies.Count > 0)
+            {
+                var enemy = enemies[0];
+                var direction = enemy.transform.position - transform.position;
+                direction.y = 0;
+                direction = direction.normalized;
+                ShootController.Instance.Shoot(direction, transform.position);
+                enemies.RemoveAt(0);
+                yield return new WaitForSeconds(ShootController.Instance.Delay);
+            }
+            isShooting = false;
+        }
+
+        if (!isShooting)
+        {
+            isShooting = true;
+            StartCoroutine(Do());
+        }
     }
     private void Dead()
     {
--- Assets/Scripts/Controllers/ShootController.cs
+++ Assets/Scripts/Controllers/ShootController.cs
@@ -8,6 +8,10 @@ public class ShootController : Singleton<ShootController>
     [SerializeField]
     private BulletController bulletPrefab;
 
+    [SerializeField]
+    private float delay;
+
+    public float Delay => delay;
     public void Shoot(Vector3 direction,Vector3 position)
     {
         var bullet = Instantiate(bulletPrefab, position, Quaternion.identity);
